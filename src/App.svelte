45e5fe4dd39159a5d9c7d0c7db15c239a8cb3e43
shawn <shawn@WKHA90106055.whq.wistron>
2022-11-04 05:52:52 +0000

    change routify hash

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,10 @@
 import { Router } from '@roxi/routify';
 import { routes } from '../.routify/routes';
 const config = {
-	urlTransform: {
-		apply: (url) => `${'/toriko'}${url}`, //external URL
-		remove: (url) => url.replace('/toriko', ''), //internal URL
-	},
+  urlTransform: {
+    apply: (url) => `${'/toriko/#'}${url}`, //external URL
+    remove: (url) => url.replace('/toriko/#', ''), //internal URL
+  },
 };
 </script>
 
