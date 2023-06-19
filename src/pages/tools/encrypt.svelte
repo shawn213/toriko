1bf7371163ac890f5c7232e03309ab81eb523dd9
shawn <shawn@WKHA90106055.whq.wistron>
2023-06-19 08:44:37 +0000

    加入 base64 加解密

--- src/pages/tools/encrypt.svelte
+++ src/pages/tools/encrypt.svelte
@@ -5,35 +5,67 @@ import {
   Button,
   Toggle,
   Input,
+  Select
 } from 'flowbite-svelte';
 import _ from 'lodash';
 import Rabbit from 'crypto-js/rabbit';
 import encUtf8 from 'crypto-js/enc-utf8';
 
-const rows: number = 10
+const rows: number = 10;
+const encrypts = [
+  {value:"default", name: "default"},
+  {value:"base64", name: "base64"},
+];
 let toggle: boolean = false;
 let content: string = '';
 let target: string = '';
 let key: string = '';
+let selected: string = 'default';
 
-$: buttonText = toggle ? '解密' : '加密';
-$: contentText = toggle ? '密文' : '需加密內容';
-$: contentPlaceholder = toggle ? '例: U2FsdGVkX18cbHi2CxrSUnAEhwx+0g==' : '內容';
+let buttonText: string = '解密';
+let contentText = '密文';
+let contentPlaceholder = '例: U2FsdGVkX18cbHi2CxrSUnAEhwx+0g==';
+$: if (toggle) {
+  buttonText = '解密';
+  contentText = '密文';
+  contentPlaceholder = '例: U2FsdGVkX18cbHi2CxrSUnAEhwx+0g==';
+} else {
+  buttonText = '加密';
+  contentText = '需加密內容';
+  contentPlaceholder = '內容';
+}
 
 const handleClick = () => {
   if (toggle) {
-    const decrypted = Rabbit.decrypt(content, key);
-    target = decrypted.toString(encUtf8);
+    switch (selected) {
+      case 'base64':
+        target = window.atob(content);
+        break;
+      default:
+        const decrypted = Rabbit.decrypt(content, key);
+        target = decrypted.toString(encUtf8);
+        break;
+    }
   } else {
-    target = Rabbit.encrypt(content, key);
+    switch (selected) {
+      case 'base64':
+        target = window.btoa(content);
+        break;
+      default:
+        target = Rabbit.encrypt(content, key);
+        break;
+    }
   }
 };
 </script>
 
-<div class="flex gap-2 mb-2 h-8">
-  <div class="dark:text-white whitespace-nowrap">加密</div>
-  <Toggle bind:checked={toggle}/>
-  <div class="dark:text-white whitespace-nowrap">解密</div>
+<div class="grid items-center sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-10 2xl:grid-cols-10 gap-4">
+  <div class="flex">
+    <div class="dark:text-white whitespace-nowrap mr-2">加密</div>
+    <Toggle bind:checked={toggle}/>
+    <div class="dark:text-white whitespace-nowrap">解密</div>
+  </div>
+  <Select class="mt-2" items={encrypts} bind:value={selected} />
 </div>
 <div class="grid items-center sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 gap-4">
   <div class="mb-6">
