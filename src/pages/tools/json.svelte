5742bb049e2b5861192ad5537a338a546873b69c
Shawn <shawn21364@gmail.com>
2023-10-20 16:33:55 +0800

    修正格式讓 json format 美化

--- src/pages/tools/json.svelte
+++ src/pages/tools/json.svelte
@@ -4,26 +4,52 @@ import _ from 'lodash';
 import { JSONEditor } from 'svelte-jsoneditor';
 
 const rows: number = 16;
-let content = '';
+let minify = '';
 let target = undefined;
 const handleClick = () => {
-  const myJSON = JSON.parse(content);
   target = {
-    json: myJSON,
+    text: minify,
+    json: JSON.parse(minify),
   };
+};
+
+const handleChange = (flag) => {
+  switch (flag) {
+    case 'minify':
+      minify = JSON.stringify(target.json, null, 0);
+      break;
+    default:
+      minify = JSON.stringify(target.json, null, '\t');
+      break;
+  }
 };
 </script>
 
-<div class="grid items-center sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 gap-4">
-  <div class="mb-6">
+<div class="grid items-center grid-cols-11 gap-4">
+  <div class="mb-6 col-span-5">
     <Label for="content-input" class="block mb-2">minify</Label>
-    <Textarea id="content-input" placeholder={JSON.stringify({ key: 'test' })} bind:value={content} {rows} />
+    <Textarea
+      id="content-input"
+      placeholder={JSON.stringify({ key: 'test' })}
+      bind:value={minify}
+      {rows}
+      style="height: 500px;" />
   </div>
-  <div class="mb-6">
+  <div class="item-center justify-center flex">
+    <div class="block">
+      <div class="mb-2">
+        <Button on:click={() => handleChange('')}>{'<'}</Button>
+        <Button on:click={handleClick}>{'>'}</Button>
+      </div>
+      <div class="justify-center flex">
+        <Button on:click={() => handleChange('minify')}>minify</Button>
+      </div>
+    </div>
+  </div>
+  <div class="mb-6 col-span-5">
     <Label for="default-input" class="block mb-2">格式化後</Label>
-    <p class="dark:text-white"><JSONEditor bind:content={target} /></p>
+    <p class="dark:text-white" style="height: 500px;">
+      <JSONEditor bind:content={target} mainMenuBar={false} navigationBar={false} />
+    </p>
   </div>
-</div>
-<div class="flex justify-center mb-2">
-  <Button on:click={handleClick}>格式化</Button>
 </div>
