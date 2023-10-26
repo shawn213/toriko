2b37fbba114b0419640b6b9d4f112dfef7053ad7
Shawn <shawn21364@gmail.com>
2023-10-26 18:11:08 +0800

    修復漲幅計算公式

--- src/pages/tools/magnitudes.svelte
+++ src/pages/tools/magnitudes.svelte
@@ -4,12 +4,12 @@ import { Input, Label } from 'flowbite-svelte';
 let now = 0;
 let next = 0;
 let magn = 0;
-let mag = '';
+let mag = 0;
 let result = 0;
 let month = 0;
 let bonus = 0;
 $: if (now && next) {
-  mag = `${Math.round((1 - now / next) * 100 * 10) / 10}%`;
+  mag = Math.round(((next - now) / now) * 100 * 10) / 10;
 }
 $: if (now && magn) {
   result = Number(now) + now * (magn / 100);
@@ -26,7 +26,8 @@ $: if (now && magn) {
     <Input type="text" class="text-right" bind:value={next} />
   </div>
   <div>
-    <Label for="last_name" class="mb-2">{mag}</Label>
+    <Label for="last_name" class="mb-2">結果(%)</Label>
+    <Input type="text" class="text-right" bind:value={mag} />
   </div>
 </div>
 <div class="grid gap-6 mb-4 md:grid-cols-6 items-center">
