a4c6b1c0991a8572b6c3c7bcf48240cdc1e991fe
Shawn <shawn21364@gmail.com>
2024-04-14 20:13:57 +0800

    修正 order 功能部分 bug

--- src/pages/order/[hashCode].svelte
+++ src/pages/order/[hashCode].svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
-import { onMount } from 'svelte';
+import { onMount, onDestroy } from 'svelte';
 import { sineIn } from 'svelte/easing';
-import { AccordionItem, Accordion, List, Li, Drawer, Span, Img, Button, Badge, Label, Input } from 'flowbite-svelte';
+import {
+  AccordionItem,
+  Accordion,
+  List,
+  Li,
+  Drawer,
+  Span,
+  Img,
+  Button,
+  Label,
+  Input,
+  Helper,
+  P,
+} from 'flowbite-svelte';
 import { Icon } from 'flowbite-svelte-icons';
 import * as _ from 'lodash';
 import { params } from '@roxi/routify';
@@ -9,13 +22,15 @@ import OrderName from '../../lib/OrderName.svelte';
 import axios from '../../utils/GoogleApi';
 import { loading, showMsg, hideHeader } from '../../stores';
 import db from '../../utils/Database';
+import * as yup from 'yup';
+import { Form, Message } from 'svelte-yup';
 
 let hidden = true;
 let total = 0;
 let fullName = '';
 let order = {
   menuUrl: '',
-  orderName: '',
+  orderOwner: '',
   status: '',
   storeName: '',
   items: [{ name: '', size: '', option: '', count: 1, price: 0 }],
@@ -29,76 +44,117 @@ let oriOrder = {
 };
 let isOwner = false;
 
-const triggerDrawer = () => {
-  hidden = !hidden;
-};
+let orderItem = { items: [{ name: '', size: '', option: '', count: 1, price: 0 }] };
+let schema = yup.object().shape({
+  items: yup
+    .array()
+    .transform((field) => {
+      if (field.length !== 1) {
+        field = field.slice(0, -1);
+      }
+      return field;
+    })
+    .of(
+      yup.object().shape({
+        name: yup.string().required('品項名稱必填'),
+        option: yup.string(),
+        size: yup.string(),
+        count: yup.number().min(1).required('數量必填'),
+        price: yup.number().min(0).required('價格必填,如只是附加品項請填0'),
+      }),
+    ),
+});
 
-$: if (order.items && order.items.every((item) => item.name)) {
-  order.items.push({
+let submitted = false;
+
+$: if (orderItem.items && orderItem.items.every((item) => item.name)) {
+  orderItem.items.push({
     name: '',
     size: '',
     option: '',
     price: 0,
     count: 1,
   });
-} else {
-  let size = order.items.filter((item) => item.name === '');
+  submitted = false;
+} else if (orderItem.items) {
+  let size = orderItem.items.filter((item) => item.name === '');
   if (size.length > 1) {
-    order.items.pop();
+    orderItem.items.pop();
   }
 }
 
-$: if (order.items.some((item) => item.price)) {
-  total = _.sumBy(order.items, 'price');
+$: if (orderItem.items && orderItem.items.some((item) => item.price)) {
+  total = _.sumBy(orderItem.items, (item) => Number(item.price) * Number(item.count));
 }
+
+const handleQuery = () => {
+  const orderName = db.getOrderUserName();
+  fullName = orderName.fullName;
+  loading.update(() => true);
+  setTimeout(() => {
+    axios
+      .post('getOrder', {
+        sheetName: $params.hashCode,
+      })
+      .then((res) => {
+        if (res.data.message === 'success') {
+          let { result } = res.data;
+          let items = [{ name: '', size: '', option: '', count: 1, price: 0 }];
+          if (!_.isEmpty(result.items)) {
+            oriOrder = _.cloneDeep(result);
+            if (result.items[orderName.name]) {
+              items = result.items[orderName.name];
+            }
+          }
+          order = { ...result, items };
+          orderItem.items = items;
+        }
+      })
+      .finally(() => {
+        loading.update(() => false);
+      });
+  }, 800);
+};
+
+const triggerDrawer = () => {
+  hidden = !hidden;
+  if (!hidden) {
+    handleQuery();
+  }
+};
 
 onMount(async () => {
   hideHeader.update(() => true);
   const orderName = db.getOrderUserName();
   fullName = orderName.fullName;
   isOwner = await db.isOrderOwner($params.hashCode);
-  loading.update(() => true);
-  axios
-    .post('getOrder', {
-      sheetName: $params.hashCode,
-    })
-    .then((res) => {
-      if (res.data.message === 'success') {
-        let { result } = res.data;
-        if (_.isEmpty(result.items)) {
-          result.items = [{ name: '', size: '', option: '', count: 1, price: 0 }];
-        } else {
-          oriOrder = _.cloneDeep(result);
-          result.items = result.items[orderName.fullName];
-        }
-        order = result;
-      }
-    })
-    .finally(() => {
-      loading.update(() => false);
-    });
+  handleQuery();
 });
 
 const handleSubmit = () => {
-  const { items } = order;
-  const its = items.filter((item) => item.name && item.size && item.count && item.price);
-  const orderName = db.getOrderUserName();
-  if (its.length > 0) {
-    loading.update(() => true);
-    axios
-      .post('addOrder', {
-        sheetName: $params.hashCode,
-        name: orderName.fullName,
-        items: its,
-      })
-      .then((res) => {
-        if (res.data.message) {
-          showMsg.set(res.data.message);
-        }
-      })
-      .finally(() => {
-        loading.update(() => false);
-      });
+  submitted = true;
+  let isValid = schema.isValidSync(orderItem);
+  if (isValid) {
+    const orderName = db.getOrderUserName();
+    const { items: its } = orderItem;
+    if (its.length > 0) {
+      loading.update(() => true);
+      axios
+        .post('addOrder', {
+          sheetName: $params.hashCode,
+          name: orderName.fullName,
+          items: its,
+        })
+        .then((res) => {
+          if (res.data.message) {
+            showMsg.set(res.data.message);
+            submitted = false;
+          }
+        })
+        .finally(() => {
+          loading.update(() => false);
+        });
+    }
   }
 };
 
@@ -136,54 +192,73 @@ const handleDelete = () => {
       loading.update(() => false);
     });
 };
+
+onDestroy(() => {
+  hideHeader.update(() => false);
+});
 </script>
 
 <OrderName />
 <div class="grid sm:grid-cols-1 lg:grid-cols-2 gap-4">
   <div class="block">
-    <Badge href={order.menuUrl}>{order.menuUrl}</Badge>
+    <a
+      href={order.menuUrl}
+      class="inline-flex items-center hover:underline text-primary-600 dark:text-primary-500"
+      target="_blank">{order.storeName}</a>
+    <P>
+      團主: <Span decorationClass="decoration-blue-500 decoration-double">{order.orderOwner}</Span>
+    </P>
     {#if order.menuUrl}
       <Img src={order.menuUrl} alt="Preview" />
     {/if}
   </div>
   <div>
     <div class="grid sm:grid-cols-1 lg:grid-cols-1 gap-4">
-      <div class="block">
-        <div class="grid sm:grid-cols-1 lg:grid-cols-4 gap-2 mb-4">
-          <Button size="xs" on:click={handleSubmit}>送出</Button>
-          <Span>總共: {total} 元</Span>
-        </div>
-        {#if isOwner}
+      <Form class="flex flex-col space-y-3" {schema} fields={orderItem} submitHandler={handleSubmit} {submitted}>
+        <div class="block">
           <div class="grid sm:grid-cols-1 lg:grid-cols-4 gap-2 mb-4">
-            <Button size="xs" on:click={handleClose}>收單</Button>
-            <Button size="xs" on:click={handleDelete}>結清</Button>
+            <Button size="xs" on:click={handleSubmit}>送出</Button>
+            <Span>總共: {total} 元</Span>
           </div>
-        {/if}
-        {#each order.items as item}
-          <div class="grid sm:grid-cols-1 lg:grid-cols-5 gap-2 mb-4 pb-1 border border-sky-500 p-1">
-            <Label class="space-y-2">
-              <span>品項名稱</span>
-              <Input bind:value={item.name} placeholder="珍珠奶茶" />
-            </Label>
-            <Label class="space-y-2">
-              <span>選項</span>
-              <Input bind:value={item.option} placeholder="多冰/糖正常" />
-            </Label>
-            <Label class="space-y-2">
-              <span>Size</span>
-              <Input bind:value={item.size} placeholder="ex:L or M or None" />
-            </Label>
-            <Label class="space-y-2">
-              <span>數量</span>
-              <Input type="number" bind:value={item.count} placeholder="1" />
-            </Label>
-            <Label class="space-y-2">
-              <span>價錢</span>
-              <Input type="number" bind:value={item.price} placeholder="1" />
-            </Label>
-          </div>
-        {/each}
-      </div>
+          {#if isOwner}
+            <div class="grid sm:grid-cols-1 lg:grid-cols-4 gap-2 mb-4">
+              <Button size="xs" on:click={handleClose}>收單</Button>
+              <Button size="xs" on:click={handleDelete}>結清</Button>
+            </div>
+          {/if}
+          {#each orderItem.items as item, idx}
+            <div class="grid sm:grid-cols-1 lg:grid-cols-5 gap-2 mb-4 pb-1 border border-sky-500 p-1">
+              <Label class="space-y-2">
+                <span>品項名稱</span>
+                <Input bind:value={item.name} placeholder="珍珠奶茶" />
+              </Label>
+              <Label class="space-y-2">
+                <span>選項</span>
+                <Input bind:value={item.option} placeholder="多冰/糖正常" />
+              </Label>
+              <Label class="space-y-2">
+                <span>Size</span>
+                <Input bind:value={item.size} placeholder="ex:L or M or None" />
+              </Label>
+              <Label class="space-y-2">
+                <span>數量</span>
+                <Input type="number" bind:value={item.count} placeholder="1" />
+              </Label>
+              <Label class="space-y-2">
+                <span>價錢</span>
+                <Input type="number" bind:value={item.price} placeholder="1" />
+              </Label>
+              <div>
+                <Helper color="red"><Message name={`items[${idx}].name`} /></Helper>
+                <Helper color="red"><Message name={`items[${idx}].option`} /></Helper>
+                <Helper color="red"><Message name={`items[${idx}].size`} /></Helper>
+                <Helper color="red"><Message name={`items[${idx}].count`} /></Helper>
+                <Helper color="red"><Message name={`items[${idx}].price`} /></Helper>
+              </div>
+            </div>
+          {/each}
+        </div>
+      </Form>
     </div>
   </div>
   <Span class="fixed right-5 top-15"><Icon name="cart-outline" class="w-5 h-5" on:click={triggerDrawer} /></Span>
